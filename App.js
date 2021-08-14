03fc90e15166ad885f2da441e929a7d797b6d72a
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2021-08-14 23:13:25 +0600

    simple design issue learing for native app

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <SafeAreaView style={styles.container}>
+        <View style={styles.avatar_container}>
+          <View style={{alignItems:'center',justifyContent:'center',color:'black',marginTop: 10}}>
+            <Image style={{marginBottom:20}} source={require("./assets/batman.png")} />
+            <Text>Welcome to First Native App</Text>
+          </View>
+        </View>
+        <View style={styles.detail_container}>
+          <View style={styles.item}>
+            <Text style={styles.attribute}>Name:</Text>
+            <Text style={styles.data}>Md. Mahidul Islam</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.attribute}>Age:</Text>
+            <Text style={styles.data}>23</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.attribute}>Designation:</Text>
+            <Text style={styles.data}>{`Software 
+            Engineer`}</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ccc',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // marginTop: 250
   },
+  avatar_container:{
+    flex: 0.4,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  detail_container:{
+    flex: 0.6,
+    backgroundColor: 'white'
+  },
+  item:{
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    flex: 0.25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 15
+  },
+  attribute:{
+    flex: 0.8,
+    color: 'orange',
+    fontSize: 30,
+  },
+  data:{
+    color: 'blue',
+    fontSize: 25,
+    fontFamily: 'italic'
+  }
 });
